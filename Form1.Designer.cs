3d6b9802e12e423c84ccc41f0c197777d373017e
Michał Świątczak <swiatczak21536@student.pswbp.pl>
2021-11-15 23:12:55 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            resources.GetString("listBox1.Items"),
+            resources.GetString("listBox1.Items1"),
+            resources.GetString("listBox1.Items2"),
+            resources.GetString("listBox1.Items3"),
+            resources.GetString("listBox1.Items4"),
+            resources.GetString("listBox1.Items5"),
+            resources.GetString("listBox1.Items6"),
+            resources.GetString("listBox1.Items7"),
+            resources.GetString("listBox1.Items8"),
+            resources.GetString("listBox1.Items9"),
+            resources.GetString("listBox1.Items10"),
+            resources.GetString("listBox1.Items11"),
+            resources.GetString("listBox1.Items12"),
+            resources.GetString("listBox1.Items13"),
+            resources.GetString("listBox1.Items14"),
+            resources.GetString("listBox1.Items15"),
+            resources.GetString("listBox1.Items16"),
+            resources.GetString("listBox1.Items17"),
+            resources.GetString("listBox1.Items18"),
+            resources.GetString("listBox1.Items19"),
+            resources.GetString("listBox1.Items20"),
+            resources.GetString("listBox1.Items21"),
+            resources.GetString("listBox1.Items22"),
+            resources.GetString("listBox1.Items23"),
+            resources.GetString("listBox1.Items24"),
+            resources.GetString("listBox1.Items25"),
+            resources.GetString("listBox1.Items26"),
+            resources.GetString("listBox1.Items27"),
+            resources.GetString("listBox1.Items28"),
+            resources.GetString("listBox1.Items29")});
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox2
+            // 
+            resources.ApplyResources(this.listBox2, "listBox2");
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            resources.GetString("listBox2.Items"),
+            resources.GetString("listBox2.Items1"),
+            resources.GetString("listBox2.Items2"),
+            resources.GetString("listBox2.Items3"),
+            resources.GetString("listBox2.Items4"),
+            resources.GetString("listBox2.Items5"),
+            resources.GetString("listBox2.Items6"),
+            resources.GetString("listBox2.Items7"),
+            resources.GetString("listBox2.Items8"),
+            resources.GetString("listBox2.Items9"),
+            resources.GetString("listBox2.Items10"),
+            resources.GetString("listBox2.Items11"),
+            resources.GetString("listBox2.Items12"),
+            resources.GetString("listBox2.Items13"),
+            resources.GetString("listBox2.Items14"),
+            resources.GetString("listBox2.Items15"),
+            resources.GetString("listBox2.Items16"),
+            resources.GetString("listBox2.Items17"),
+            resources.GetString("listBox2.Items18"),
+            resources.GetString("listBox2.Items19"),
+            resources.GetString("listBox2.Items20"),
+            resources.GetString("listBox2.Items21"),
+            resources.GetString("listBox2.Items22"),
+            resources.GetString("listBox2.Items23"),
+            resources.GetString("listBox2.Items24"),
+            resources.GetString("listBox2.Items25"),
+            resources.GetString("listBox2.Items26"),
+            resources.GetString("listBox2.Items27"),
+            resources.GetString("listBox2.Items28")});
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            resources.ApplyResources(this.listBox3, "listBox3");
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Items.AddRange(new object[] {
+            resources.GetString("listBox3.Items"),
+            resources.GetString("listBox3.Items1"),
+            resources.GetString("listBox3.Items2"),
+            resources.GetString("listBox3.Items3"),
+            resources.GetString("listBox3.Items4"),
+            resources.GetString("listBox3.Items5"),
+            resources.GetString("listBox3.Items6"),
+            resources.GetString("listBox3.Items7"),
+            resources.GetString("listBox3.Items8"),
+            resources.GetString("listBox3.Items9"),
+            resources.GetString("listBox3.Items10"),
+            resources.GetString("listBox3.Items11"),
+            resources.GetString("listBox3.Items12"),
+            resources.GetString("listBox3.Items13"),
+            resources.GetString("listBox3.Items14"),
+            resources.GetString("listBox3.Items15"),
+            resources.GetString("listBox3.Items16"),
+            resources.GetString("listBox3.Items17"),
+            resources.GetString("listBox3.Items18"),
+            resources.GetString("listBox3.Items19"),
+            resources.GetString("listBox3.Items20"),
+            resources.GetString("listBox3.Items21"),
+            resources.GetString("listBox3.Items22"),
+            resources.GetString("listBox3.Items23"),
+            resources.GetString("listBox3.Items24"),
+            resources.GetString("listBox3.Items25"),
+            resources.GetString("listBox3.Items26"),
+            resources.GetString("listBox3.Items27"),
+            resources.GetString("listBox3.Items28")});
+            this.listBox3.Name = "listBox3";
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1")});
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Label2
+            // 
+            resources.ApplyResources(this.Label2, "Label2");
+            this.Label2.Name = "Label2";
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button9
+            // 
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.Name = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            resources.ApplyResources(this.button10, "button10");
+            this.button10.Name = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Dictionary<string, string> myDict =
+            new Dictionary<string, string>();
+
+            myDict.Add("Pies", "Dog");
+
+            myDict.Add("Kot", "Cat");
+
+            myDict.Add("Swinia", "Pig");
+
+            myDict.Add("Chomik", "Hamster");
+
+            myDict.Add("Ptak", "Bird");
+
+            myDict.Add("Mysz", "Mouse");
+
+            myDict.Add("Pszczola", "Bee");
+
+            myDict.Add("Niedzwiedz", "Bear");
+
+            myDict.Add("Krowa", "Cow");
+
+            myDict.Add("Kurczak", "Chicken");
+
+            myDict.Add("Kaczka", "Duck");
+
+            myDict.Add("Slon", "Elephant");
+
+            myDict.Add("Ryba", "Fish");
+
+            myDict.Add("Zaba", "Frog");
+
+            myDict.Add("Lew", "Lion");
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox2.Visible = false;
+            listBox3.Visible = false;
+            listBox1.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Visible = false;
+            listBox2.Visible = false;
+            listBox3.Visible = false;
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox2.Visible = true;
+            listBox3.Visible = false;
+            listBox1.Visible = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listBox2.Visible = false;
+            listBox3.Visible = true;
+            listBox1.Visible = false;
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("pl");
+                    break;
+                case 1:
+                    Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+                    break;
+            }
+
+            this.Controls.Clear();
+            InitializeComponent();
+
+            Dictionary<string, string> myDict =
+            new Dictionary<string, string>();
+
+            myDict.Add("Pies", "Dog");
+
+            myDict.Add("Kot", "Cat");
+
+            myDict.Add("Swinia", "Pig");
+
+            myDict.Add("Chomik", "Hamster");
+
+            myDict.Add("Ptak", "Bird");
+
+            myDict.Add("Mysz", "Mouse");
+
+            myDict.Add("Pszczola", "Bee");
+
+            myDict.Add("Niedzwiedz", "Bear");
+
+            myDict.Add("Krowa", "Cow");
+
+            myDict.Add("Kurczak", "Chicken");
+
+            myDict.Add("Kaczka", "Duck");
+
+            myDict.Add("Slon", "Elephant");
+
+            myDict.Add("Ryba", "Fish");
+
+            myDict.Add("Zaba", "Frog");
+
+            myDict.Add("Lew", "Lion");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> myDict =
+            new Dictionary<string, string>();
+
+
+            myDict.Add("Dog", "Pies");
+
+            myDict.Add("Cat", "Kot");
+
+            myDict.Add("Pig", "Swinia");
+
+            myDict.Add("Hamster", "Chomik");
+
+            myDict.Add("Bird", "Ptak");
+
+            myDict.Add("Mouse", "Mysz");
+
+            myDict.Add("Bee", "Pszczola");
+
+            myDict.Add("Bear", "Niedzwiedz");
+
+            myDict.Add("Cow", "Krowa");
+
+            myDict.Add("Chicken", "Kurczak");
+
+            myDict.Add("Duck", "Kaczka");
+
+            myDict.Add("Elephant", "Slon");
+
+            myDict.Add("Fish", "Ryba");
+
+            myDict.Add("Frog", "Zaba");
+
+            myDict.Add("Lion", "Lew");
+
+            if (textBox2.Text == myDict[textBox1.Text])
+            {
+                MessageBox.Show("Dobrze");
+                var rnd = new Random();
+                int randomValue = rnd.Next(1, 15);
+                string b = myDict.Values.ElementAt(randomValue);
+                textBox2.Text = b;
+                textBox1.Text = "";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> myDict =
+            new Dictionary<string, string>();
+
+            myDict.Add("Pies", "Dog");
+
+            myDict.Add("Kot", "Cat");
+
+            myDict.Add("Swinia", "Pig");
+
+            myDict.Add("Chomik", "Hamster");
+
+            myDict.Add("Ptak", "Bird");
+
+            myDict.Add("Mysz", "Mouse");
+
+            myDict.Add("Pszczola", "Bee");
+
+            myDict.Add("Niedzwiedz", "Bear");
+
+            myDict.Add("Krowa", "Cow");
+
+            myDict.Add("Kurczak", "Chicken");
+
+            myDict.Add("Kaczka", "Duck");
+
+            myDict.Add("Slon", "Elephant");
+
+            myDict.Add("Ryba", "Fish");
+
+            myDict.Add("Zaba", "Frog");
+
+            myDict.Add("Lew", "Lion");
+
+
+            if (textBox2.Text == myDict[textBox1.Text])
+            {
+                MessageBox.Show("Dobrze");
+                var rnd = new Random();
+                int randomValue = rnd.Next(1, 15);
+                string a = myDict.Values.ElementAt(randomValue);
+                textBox2.Text = a;
+                textBox1.Text = "";
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> myDict =
+            new Dictionary<string, string>();
+
+            myDict.Add("Pies", "Dog");
+            myDict.Add("Dog", "Pies");
+
+            myDict.Add("Kot", "Cat");
+            myDict.Add("Cat", "Kot");
+
+            myDict.Add("Swinia", "Pig");
+            myDict.Add("Pig", "Swinia");
+
+            myDict.Add("Chomik", "Hamster");
+            myDict.Add("Hamster", "Chomik");
+
+            myDict.Add("Ptak", "Bird");
+            myDict.Add("Bird", "Ptak");
+
+            myDict.Add("Mysz", "Mouse");
+            myDict.Add("Mouse", "Mysz");
+
+            myDict.Add("Pszczola", "Bee");
+            myDict.Add("Bee", "Pszczola");
+
+            myDict.Add("Niedzwiedz", "Bear");
+            myDict.Add("Bear", "Niedzwiedz");
+
+            myDict.Add("Krowa", "Cow");
+            myDict.Add("Cow", "Krowa");
+
+            myDict.Add("Kurczak", "Chicken");
+            myDict.Add("Chicken", "Kurczak");
+
+            myDict.Add("Kaczka", "Duck");
+            myDict.Add("Duck", "Kaczka");
+
+            myDict.Add("Slon", "Elephant");
+            myDict.Add("Elephant", "Slon");
+
+            myDict.Add("Ryba", "Fish");
+            myDict.Add("Fish", "Ryba");
+
+            myDict.Add("Zaba", "Frog");
+            myDict.Add("Frog", "Zaba");
+
+            myDict.Add("Lew", "Lion");
+            myDict.Add("Lion", "Lew");
+
+            if (textBox2.Text == myDict[textBox1.Text])
+            {
+                MessageBox.Show("Dobrze");
+                var rnd = new Random();
+                int randomValue = rnd.Next(1, 30);
+                string a = myDict.Values.ElementAt(randomValue);
+                textBox2.Text = a;
+                textBox1.Text = "";
+            }
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "Dog";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "Pies";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "Pies";
+        }
+    }
+
+}
+
